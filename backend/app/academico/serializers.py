f677f7c4867321b588e296ab0ce69f754e30e77f
dalbizurez <dalbizurez@correo.url.edu.gt>
2026-03-29 16:17:23 -0600

    fix: settings import typo and double requirements.txt

--- backend/app/academico/serializers.py
+++ backend/app/academico/serializers.py
@@ -1,4 +1,4 @@
-ffrom rest_framework import serializers
+from rest_framework import serializers
 from .models import Carrera, Pensum, Semestre, Curso
 
 class CarreraSerializer(serializers.ModelSerializer):
